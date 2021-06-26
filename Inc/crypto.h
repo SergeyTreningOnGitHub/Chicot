740dadf6a42e2ca17fae5b813ea0e76b3a7f5ad7
Sergey Palamarchuk <spalamarchuk787@gmail.com>
2021-06-26 11:35:46 +0300

    Add signing, verifying, get private and pub keys

--- Inc/crypto.h
+++ Inc/crypto.h
@@ -12,9 +12,21 @@
 
 constexpr uint32_t SHA256_SIZE = 32;
 
-using SHA256_Digest = std::array<uint8_t, SHA256_SIZE>;
+using EC_Sign    = std::vector<uint8_t>;
+using PrivateKey = std::vector<uint8_t>;
+using PublicKey  = std::vector<uint8_t>;
+using SHA256_Digest = std::vector<uint8_t>;
 using ByteMessage = std::vector<uint8_t>;
 
+int GetCurveNid(){
+    const char* curve_name = "secp256k1";
+    int curve_nid = EC_curve_nist2nid(curve_name);
+    if (curve_nid == NID_undef) {
+        // try converting the shortname (sn) to nid (numberic id)
+        curve_nid = OBJ_sn2nid(curve_name);
+    }
+    return curve_nid;
+}
 
 SHA256_Digest GenDigest(const ByteMessage& msg){        
     assert(EVP_MD_size(EVP_sha256()) == SHA256_SIZE);
@@ -29,4 +41,29 @@ SHA256_Digest GenDigest(const ByteMessage& msg){
     EVP_DigestFinal_ex(mdctx, res.data(), &fictive_len);
     EVP_MD_CTX_free(mdctx);
     return res;
+}
+
+bool VerifySign(const ByteMessage& msg, const EcSign& sig, const PublicKey& pub_key){
+    int curve_nid = GetCurveNid();
+
+    EVP_PKEY* pub_key_formed = EVP_PKEY_new_raw_public_key(curve_nid, NULL, pub_key.data(), pub_key.size());
+
+    if(1 != EVP_DigestVerifyInit(mdctx, NULL, EVP_sha256(), NULL, pub_key_formed)){ 
+        goto err;
+    }
+
+    if(1 != EVP_DigestVerifyUpdate(mdctx, msg.data(), msg.size())){
+         goto err;
+    }
+
+    if(1 == EVP_DigestVerifyFinal(mdctx, sig.data(), sig.size()))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    EVP_PKEY_free(pub_key_formed);
 }
--- Inc/wallet.h
+++ Inc/wallet.h
@@ -1,24 +1,12 @@
 #pragma once
 #include "crypto.h"
 
-
-constexpr uint32_t SHA256_SIZE = 32;
-
-using SHA256_Digest = std::array<uint8_t, SHA256_SIZE>;
-using ByteMessage = std::vector<uint8_t>;
-
-
 class Wallet{    
     EVP_PKEY* params_;
-    EC_KEY* ec_key_;
+    EVP_PKEY* ec_key_;
 
     void gen_key_params(){
-        const char* curve_name = "secp256k1";
-        int curve_nid = EC_curve_nist2nid(curve_name);
-        if (curve_nid == NID_undef) {
-            // try converting the shortname (sn) to nid (numberic id)
-            curve_nid = OBJ_sn2nid(curve_name);
-        }
+        int curve_nid = GetCurveNid();
         
         /* Create the context for generating the parameters */
         EVP_PKEY_CTX* pctx;
@@ -42,22 +30,21 @@ class Wallet{
 
         if(!EVP_PKEY_keygen_init(key_ctx)) 
             goto err;
-
-        EVP_PKEY* pkey = NULL;
+        
         /* Generate the key */
-        if (!EVP_PKEY_keygen(key_ctx, &pkey)) 
+        if (!EVP_PKEY_keygen(key_ctx, &ec_key_)) 
             goto err;
 
-        ec_key_ = EVP_PKEY_get1_EC_KEY(pkey);        
+        //ec_key_ = EVP_PKEY_get1_EC_KEY(pkey);        
     }
 
 public:
     Wallet() : params_(NULL), ec_key_(NULL){}
 
     void SavePrivate(const string& filename){
-        BIO* file = BIO_new_file((filename + ".pem").c_str(), "w");
+        BIO* file = BIO_new_file((filename + ".pem").c_str(), "w");        
 
-        int len = PEM_write_bio_ECPrivateKey(file, ec_key_, NULL, NULL, 0, NULL, NULL);
+        int len = PEM_write_bio_PrivateKey(file, key, NULL, NULL, 0, NULL, NULL);
         if (len <= 0) {
             error_and_exit("Could not write the private key");
         }
@@ -76,7 +63,7 @@ public:
 
         ERR_print_errors_fp(stdout);
 
-        assert(ed_key_ != NULL);
+        assert(ec_key_ != NULL);
 
 
         BIO_free(file);
@@ -86,5 +73,70 @@ public:
         gen_key_params();
         gen_keys();
         SavePrivate("private");
-    }    
+
+    }
+
+    EC_Sign SignMessage(const ByteMessage& msg){
+        EVP_MD_CTX *mdctx = NULL;
+        int ret = 0;
+        
+        if(!(mdctx = EVP_MD_CTX_create())) {
+            goto err;
+        }
+ 
+        if(1 != EVP_DigestSignInit(mdctx, NULL, EVP_sha256(), NULL, ec_key_)){ 
+            goto err;
+        }
+ 
+        if(1 != EVP_DigestSignUpdate(mdctx, msg.data(), msg.size())){
+             goto err;
+        }
+ 
+        EC_Sign sig_res;
+        uint32_t size_sig;
+
+        if(1 != EVP_DigestSignFinal(mdctx, NULL, &size_sig)){
+            goto err;
+        }
+
+        sig_res.resize(size_sig);
+        if(1 != EVP_DigestSignFinal(mdctx, sig_res.data(), sig_res.size())){
+            goto err;
+        }       
+
+        EVP_MD_CTX_destroy(mdctx);
+        return sig_res;
+    }
+
+    PrivateKey GetPrivateKey(){
+        PrivateKey res;
+        uint32_t priv_key_len = 0;
+        if (!EVP_PKEY_get_raw_private_key(ec_key_, NULL, &priv_key_len)){
+            return {};                        
+        }
+
+        res.resize(priv_key_len);
+
+        if(!EVP_PKEY_get_raw_private_key(ec_key_, res.data(), res.size()){
+            return {};
+        }
+        return res;
+    }
+
+    PublicKey GetPublicKey(){
+        PublicKey res;
+        uint32_t pub_key_len = 0;
+        if (!EVP_PKEY_get_raw_public_key(ec_key_, NULL, &pub_key_len)){
+            return {};
+                                      
+        }
+
+        res.resize(pub_key_len);
+
+        if(!EVP_PKEY_get_raw_pubic_key(ec_key_, res.data(), res.size()){
+            return {};
+        }
+
+        return res;
+    }        
 };
