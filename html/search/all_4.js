63f89c13286ae7d4570c6668a514fc2c226551bd
SergeyTreningOnGitHub <SergeyTreningOnGitHub@users.noreply.github.com>
2021-07-16 19:10:42 +0000

    deploy: b3ef6cd39ca7f61143caed08e201d44714246378

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['capnp_37',['capnp',['../namespacecapnp.html',1,'']]],
-  ['capnp_5fdeclare_5fschema_38',['CAPNP_DECLARE_SCHEMA',['../namespacecapnp_1_1schemas.html#aae61486307e1f0a3cbd9b9f29e7b4d6b',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(c7c05cd4e4343fdf)'],['../namespacecapnp_1_1schemas.html#a554487897d702ef04a4ebe8e8800fc03',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(a0e6a4aa33384046)'],['../namespacecapnp_1_1schemas.html#a485b5bc10e1a32348e2ac17d4d8ca966',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(85593a3f6a09e2e0)'],['../namespacecapnp_1_1schemas.html#ac623651301f191bdea5d1fdfe78fb7f7',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(a584e4b88a131c66)']]],
-  ['countinputs_39',['CountInputs',['../classTransaction.html#ab3cd203e356d7454aef8f37e59938125',1,'Transaction']]],
-  ['countoutputs_40',['CountOutputs',['../classTransaction.html#ad99619d576681a88fda8dd86f607e496',1,'Transaction']]],
-  ['crypto_2eh_41',['crypto.h',['../crypto_8h.html',1,'']]],
-  ['cur_5fblock_5f_42',['cur_block_',['../classLedger.html#ae362956c61abd222c9b3bd2ca45d4744',1,'Ledger']]],
-  ['schemas_43',['schemas',['../namespacecapnp_1_1schemas.html',1,'capnp']]]
+  ['capnp_38',['capnp',['../namespacecapnp.html',1,'']]],
+  ['capnp_5fdeclare_5fschema_39',['CAPNP_DECLARE_SCHEMA',['../namespacecapnp_1_1schemas.html#aae61486307e1f0a3cbd9b9f29e7b4d6b',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(c7c05cd4e4343fdf)'],['../namespacecapnp_1_1schemas.html#a554487897d702ef04a4ebe8e8800fc03',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(a0e6a4aa33384046)'],['../namespacecapnp_1_1schemas.html#a485b5bc10e1a32348e2ac17d4d8ca966',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(85593a3f6a09e2e0)'],['../namespacecapnp_1_1schemas.html#ac623651301f191bdea5d1fdfe78fb7f7',1,'capnp::schemas::CAPNP_DECLARE_SCHEMA(a584e4b88a131c66)']]],
+  ['countinputs_40',['CountInputs',['../classTransaction.html#ab3cd203e356d7454aef8f37e59938125',1,'Transaction']]],
+  ['countoutputs_41',['CountOutputs',['../classTransaction.html#ad99619d576681a88fda8dd86f607e496',1,'Transaction']]],
+  ['crypto_2eh_42',['crypto.h',['../crypto_8h.html',1,'']]],
+  ['cur_5fblock_5f_43',['cur_block_',['../classLedger.html#ae362956c61abd222c9b3bd2ca45d4744',1,'Ledger']]],
+  ['schemas_44',['schemas',['../namespacecapnp_1_1schemas.html',1,'capnp']]]
 ];
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -2,15 +2,16 @@ var searchData=
 [
   ['addinput_212',['AddInput',['../classTransaction.html#aedf1706f3892eaf71eefc352f7a53993',1,'Transaction']]],
   ['addoutput_213',['AddOutput',['../classTransaction.html#ad78ec04ec64052dade594f3551439f8e',1,'Transaction']]],
-  ['adoptecsign_214',['adoptEcSign',['../classTransactData_1_1Builder.html#a1c50b86292c8abbd60d435e1987352cf',1,'TransactData::Builder']]],
-  ['adoptinputs_215',['adoptInputs',['../classTransactData_1_1Builder.html#a8a897d66b751cd446a6e69680818e7bd',1,'TransactData::Builder']]],
-  ['adoptmerkleroot_216',['adoptMerkleRoot',['../classBlockData_1_1Builder.html#ab80215c31d9e3e75e29a0cf1dc0064de',1,'BlockData::Builder']]],
-  ['adoptnonce_217',['adoptNonce',['../classBlockData_1_1Builder.html#a56b26bb813f9228f0d8c4231703e2015',1,'BlockData::Builder']]],
-  ['adoptoutputs_218',['adoptOutputs',['../classTransactData_1_1Builder.html#a95a75ecfebeb3b6766799063c29fa16b',1,'TransactData::Builder']]],
-  ['adoptprevblockhash_219',['adoptPrevBlockHash',['../classBlockData_1_1Builder.html#a7d9b3c5952fbbd5dc6291ca23abac975',1,'BlockData::Builder']]],
-  ['adoptprevtxhash_220',['adoptPrevTxHash',['../classTransactData_1_1InputData_1_1Builder.html#aad23c1f5c5a13143cf47f1fdf6f34113',1,'TransactData::InputData::Builder']]],
-  ['adoptpubkey_221',['adoptPubKey',['../classTransactData_1_1OutputData_1_1Builder.html#ab2aca257a1d16a44fa4c9d825d2dcb0a',1,'TransactData::OutputData::Builder']]],
-  ['adopttxs_222',['adoptTxs',['../classBlockData_1_1Builder.html#a7546d0f31494f992492950a0e78f6b0a',1,'BlockData::Builder']]],
-  ['as_5fbytes_223',['as_bytes',['../classTransaction.html#ae86608ecfdcddc9d0ac96e7754629e26',1,'Transaction']]],
-  ['asreader_224',['asReader',['../classTransactData_1_1Builder.html#ab0f84359585fab1d6f7f129933ede1f9',1,'TransactData::Builder::asReader()'],['../classTransactData_1_1InputData_1_1Builder.html#a26ca7ccebde1c1de648599ad2bfb9271',1,'TransactData::InputData::Builder::asReader()'],['../classTransactData_1_1OutputData_1_1Builder.html#a17b461047ea3efc19bb448f94046bcc7',1,'TransactData::OutputData::Builder::asReader()'],['../classBlockData_1_1Builder.html#a936a0b192b8c46e3abc9c417bba5a497',1,'BlockData::Builder::asReader()']]]
+  ['addtx_214',['AddTx',['../classBlock.html#ab539cdbdee37f294018da37511b6543f',1,'Block']]],
+  ['adoptecsign_215',['adoptEcSign',['../classTransactData_1_1Builder.html#a1c50b86292c8abbd60d435e1987352cf',1,'TransactData::Builder']]],
+  ['adoptinputs_216',['adoptInputs',['../classTransactData_1_1Builder.html#a8a897d66b751cd446a6e69680818e7bd',1,'TransactData::Builder']]],
+  ['adoptmerkleroot_217',['adoptMerkleRoot',['../classBlockData_1_1Builder.html#ab80215c31d9e3e75e29a0cf1dc0064de',1,'BlockData::Builder']]],
+  ['adoptnonce_218',['adoptNonce',['../classBlockData_1_1Builder.html#a56b26bb813f9228f0d8c4231703e2015',1,'BlockData::Builder']]],
+  ['adoptoutputs_219',['adoptOutputs',['../classTransactData_1_1Builder.html#a95a75ecfebeb3b6766799063c29fa16b',1,'TransactData::Builder']]],
+  ['adoptprevblockhash_220',['adoptPrevBlockHash',['../classBlockData_1_1Builder.html#a7d9b3c5952fbbd5dc6291ca23abac975',1,'BlockData::Builder']]],
+  ['adoptprevtxhash_221',['adoptPrevTxHash',['../classTransactData_1_1InputData_1_1Builder.html#aad23c1f5c5a13143cf47f1fdf6f34113',1,'TransactData::InputData::Builder']]],
+  ['adoptpubkey_222',['adoptPubKey',['../classTransactData_1_1OutputData_1_1Builder.html#ab2aca257a1d16a44fa4c9d825d2dcb0a',1,'TransactData::OutputData::Builder']]],
+  ['adopttxs_223',['adoptTxs',['../classBlockData_1_1Builder.html#a7546d0f31494f992492950a0e78f6b0a',1,'BlockData::Builder']]],
+  ['as_5fbytes_224',['as_bytes',['../classTransaction.html#ae86608ecfdcddc9d0ac96e7754629e26',1,'Transaction']]],
+  ['asreader_225',['asReader',['../classTransactData_1_1Builder.html#ab0f84359585fab1d6f7f129933ede1f9',1,'TransactData::Builder::asReader()'],['../classTransactData_1_1InputData_1_1Builder.html#a26ca7ccebde1c1de648599ad2bfb9271',1,'TransactData::InputData::Builder::asReader()'],['../classTransactData_1_1OutputData_1_1Builder.html#a17b461047ea3efc19bb448f94046bcc7',1,'TransactData::OutputData::Builder::asReader()'],['../classBlockData_1_1Builder.html#a936a0b192b8c46e3abc9c417bba5a497',1,'BlockData::Builder::asReader()']]]
 ];
